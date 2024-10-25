619482c77c737fb24e47cf300e84f24bc966da38
Florian <florian.fm.martin@gmail.com>
2024-10-25 17:56:22 -0400

    fmt

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 import { Renderer } from "jsr:@libs/markdown";
 
-const help = "imd [cmd] [file]\n\tcmd\t`show` or `exec`\n\tfile\tpath to the `.imd` notebook"
-const cmds = ["show", "exec"]
+const help =
+  "imd [cmd] [file]\n\tcmd\t`show` or `exec`\n\tfile\tpath to the `.imd` notebook";
+const cmds = ["show", "exec", "edit"];
 const css = `
 :root {
     --bg: #eee;
@@ -49,74 +50,73 @@ li {
 `;
 
 const run = async (c: string) => {
-  const cs = c.split(" ")
-  const e = new Deno.Command(cs[0], { args: cs.splice(1) })
-  return await e.output()
-}
+  const cs = c.split(" ");
+  const e = new Deno.Command(cs[0], { args: cs.splice(1) });
+  return await e.output();
+};
 
 const show = async (f: string) => {
-  const content = await Deno.readTextFile(f)
-  const html = (await Renderer.render(content)).split("\n").slice(2).join("\n")
-  const htmlFile = `${f}.html`
+  const content = await Deno.readTextFile(f);
+  const html = (await Renderer.render(content)).split("\n").slice(2).join("\n");
+  const htmlFile = `${f}.html`;
   await Deno.writeTextFile(
     htmlFile,
-    `<html><body><style>${css}</style>${html}</body></html>`
-  )
-  await run(`xdg-open ./${htmlFile}`)
-}
+    `<html><body><style>${css}</style>${html}</body></html>`,
+  );
+  await run(`xdg-open ./${htmlFile}`);
+};
 
 const exec = async (f: string) => {
-  const content = await Deno.readTextFile(f)
+  const content = await Deno.readTextFile(f);
 
-  const lang = content.match(/```(.+)\n/m)?.[1]
+  const lang = content.match(/```(.+)\n/m)?.[1];
 
-  let output = ""
+  let output = "";
   let inside_code = false;
 
   for (const line of content.split("\n")) {
     if (!inside_code && line.startsWith("```")) {
-      inside_code = true
-    }
-    else if (inside_code) {
+      inside_code = true;
+    } else if (inside_code) {
       if (line.startsWith("```")) {
-        output += "\n"
-        inside_code = false
-      }
-      else {
-        output += line + "\n"
+        output += "\n";
+        inside_code = false;
+      } else {
+        output += line + "\n";
       }
     }
   }
 
-  const outfile = `${f}.${lang}`
-  await Deno.writeTextFile(outfile, output)
+  const outfile = `${f}.${lang}`;
+  await Deno.writeTextFile(outfile, output);
 
-  const command: string[] = content.match(/core: (.*)/)?.[1].replace("$FILE", `./${outfile}`).split(" ")!
+  const command: string[] = content.match(/core: (.*)/)?.[1].replace(
+    "$FILE",
+    `./${outfile}`,
+  ).split(" ")!;
   const out = new Deno.Command(command[0], { args: command.slice(1) });
   const { code, stdout, stderr } = await out.output();
   console.assert(code === 0);
   console.log(new TextDecoder().decode(stdout));
   console.error(new TextDecoder().decode(stderr));
-}
+};
 
 const main = async () => {
-  const cmd = Deno.args[0]
+  const cmd = Deno.args[0];
 
   if (!cmds.includes(cmd)) {
-    console.log(help)
-    console.log(Deno.args)
-    Deno.exit()
+    console.log(help);
+    console.log(Deno.args);
+    Deno.exit();
   }
 
-  const file = Deno.args[1]
+  const file = Deno.args[1];
 
   if (cmd == "show") {
-    await show(file)
+    await show(file);
+  } else {
+    await exec(file);
   }
-  else {
-    await exec(file)
-  }
-}
+};
 
-await main()
-
+await main();
--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
 # IMD
+
 ## Interactive MarkDown
 
 See [the main example](./examples/fibonacci.imd) and its ouputs.
@@ -6,4 +7,3 @@ See [the main example](./examples/fibonacci.imd) and its ouputs.
 Notebooks are nice, but so complex for no reasons.
 
 IMD is a simple solution.
-
