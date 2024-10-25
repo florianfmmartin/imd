11f992636e7e572100a374cd5d811f15101215a4
Florian <florian.fm.martin@gmail.com>
2024-10-25 17:55:38 -0400

    wippppp

--- main.ts
+++ main.ts
@@ -2,18 +2,66 @@ import { Renderer } from "jsr:@libs/markdown";
 
 const help = "imd [cmd] [file]\n\tcmd\t`show` or `exec`\n\tfile\tpath to the `.imd` notebook"
 const cmds = ["show", "exec"]
+const css = `
+:root {
+    --bg: #eee;
+    --fg: #333;
+    --code-bg: #ddd;
+    --accent: #797;
+    font-family: monospace;
+    font-size: 16px;
+}
+
+body {
+    background: var(--bg);
+    color: var(--fg);
+    max-width: 800px;
+    margin: auto;
+}
+
+pre > code {
+    background: var(--code-bg);
+    color: var(--accent);
+    padding: 0.75rem;
+    display: block;
+    border-radius: 0.3rem;
+    width: auto;
+    white-space: pre-wrap;
+    max-width: 100%;
+}
+
+code {
+    background: var(--code-bg);
+    color: var(--accent);
+    padding: 1px 6px;
+    border-radius: 3px;
+}
+
+.content {
+    max-width: 800px;
+    margin: auto;
+    padding: 1rem;
+}
+
+li {
+    margin-top: 0.5rem;
+}
+`;
 
 const run = async (c: string) => {
   const cs = c.split(" ")
   const e = new Deno.Command(cs[0], { args: cs.splice(1) })
-  await e.output()
+  return await e.output()
 }
 
 const show = async (f: string) => {
   const content = await Deno.readTextFile(f)
-  const html = await Renderer.render(content)
+  const html = (await Renderer.render(content)).split("\n").slice(2).join("\n")
   const htmlFile = `${f}.html`
-  await Deno.writeTextFile(htmlFile, html)
+  await Deno.writeTextFile(
+    htmlFile,
+    `<html><body><style>${css}</style>${html}</body></html>`
+  )
   await run(`xdg-open ./${htmlFile}`)
 }
 
@@ -42,6 +90,13 @@ const exec = async (f: string) => {
 
   const outfile = `${f}.${lang}`
   await Deno.writeTextFile(outfile, output)
+
+  const command: string[] = content.match(/core: (.*)/)?.[1].replace("$FILE", `./${outfile}`).split(" ")!
+  const out = new Deno.Command(command[0], { args: command.slice(1) });
+  const { code, stdout, stderr } = await out.output();
+  console.assert(code === 0);
+  console.log(new TextDecoder().decode(stdout));
+  console.error(new TextDecoder().decode(stderr));
 }
 
 const main = async () => {
