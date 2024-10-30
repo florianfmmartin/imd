65998bf122a4ca935162980ee823eb94dc132326
Florian <florian.fm.martin@gmail.com>
2024-10-30 17:19:25 -0400

    change readme, simplify the tool

--- main.ts
+++ main.ts
@@ -1,101 +1,42 @@
-import { Renderer } from "jsr:@libs/markdown";
+const f = Deno.args[0];
 
-const help =
-  "imd [cmd] [file]\n\tcmd\t`show` or `exec`\n\tfile\tpath to the `.imd` notebook";
-const cmds = ["show", "exec", "edit"];
-const css = await Deno.readTextFile("./index.css")
+const content = await Deno.readTextFile(f);
 
-const run = async (c: string) => {
-  const cs = c.split(" ");
-  const e = new Deno.Command(cs[0], { args: cs.splice(1) });
-  return await e.output();
-};
+const lang = content.match(/```(.+)\n/m)?.[1];
 
-const show = async (f: string) => {
-  const content = await Deno.readTextFile(f);
-  const html = (await Renderer.render(content)).split("\n").slice(2).join("\n");
-  const htmlFile = `${f}.html`;
-  await Deno.writeTextFile(
-    htmlFile,
-    `<html><body><style>${css}</style>${html}</body></html>`,
-  );
-  await run(`xdg-open ./${htmlFile}`);
-};
+let output = "";
+let inside_code = false;
 
-const exec = async (f: string) => {
-  const content = await Deno.readTextFile(f);
-
-  const lang = content.match(/```(.+)\n/m)?.[1];
-
-  let output = "";
-  let inside_code = false;
-
-  for (const line of content.split("\n")) {
-    if (!inside_code && line.startsWith("```")) {
-      inside_code = true;
-    } else if (inside_code) {
-      if (line.startsWith("```")) {
-        output += "\n";
-        inside_code = false;
-      } else {
-        output += line + "\n";
-      }
+for (const line of content.split("\n")) {
+  if (!inside_code && line.startsWith("```")) {
+    inside_code = true;
+  } else if (inside_code) {
+    if (line.startsWith("```")) {
+      output += "\n";
+      inside_code = false;
+    } else {
+      output += line + "\n";
     }
   }
+}
 
-  const outfile = `${f}.${lang}`;
-  await Deno.writeTextFile(outfile, output);
+const outfile = `${f}.${lang}`;
+await Deno.writeTextFile(outfile, output);
 
-  const command: string[] = content.match(/core: (.*)/)?.[1].replace(
-    "$FILE",
-    `./${outfile}`,
-  ).split(" ")!;
+// TODO: match --- 's to make sur we're in frontmatter
+const command: string[] = content.match(/core: (.*)/)?.[1].replace(
+  "$FILE",
+  `./${outfile}`,
+).split(" ")!;
 
-  const out = new Deno.Command(command[0], { args: command.slice(1) });
-  const { code, stdout, stderr } = await out.output();
+const out = new Deno.Command(command[0], { args: command.slice(1) });
+const { code, stdout, stderr } = await out.output();
 
-  if (code === 0) {
-    console.log("Success :)");
-    console.log(new TextDecoder().decode(stdout));
-  } else {
-    console.log("Failed :(");
-    console.log(new TextDecoder().decode(stderr));
-  }
-};
+if (code === 0) {
+  console.log("Success :)");
+  console.log(new TextDecoder().decode(stdout));
+} else {
+  console.log("Failed :(");
+  console.log(new TextDecoder().decode(stderr));
+}
 
-const edit = async (f: string) => {
-  const server = Deno.serve((req) => {
-    const path = req.url.split("/").slice(3)
-    if (path.length == 0) {
-      // HOME
-    }
-    if (path[0] == "exec") {
-      // exec code
-    }
-  });
-  const { hostname, port } = server.addr
-  const url = `http://${hostname}:${port}`
-  await run(`xdg-open ${url}`);
-};
-
-const main = async () => {
-  const cmd = Deno.args[0];
-
-  if (!cmds.includes(cmd)) {
-    console.log(help);
-    console.log(Deno.args);
-    Deno.exit();
-  }
-
-  const file = Deno.args[1];
-
-  if (cmd == "show") {
-    await show(file);
-  } else if (cmd == "exec") {
-    await exec(file);
-  } else if (cmd == "edit") {
-    await edit(file)
-  }
-};
-
-await main();
--- readme.md
+++ readme.md
@@ -1,9 +1,22 @@
 # IMD
 
-## Interactive MarkDown
+Interactive MarkDown
 
-See [the main example](./examples/fibonacci.imd) and its ouputs.
+## Why?
 
 Notebooks are nice, but so complex for no reasons.
 
-IMD is a simple solution.
+## Examples
+
+See [the main example](./examples/fibonacci.imd) and its ouputs.
+
+### IMD is a simple solution
+
+1. Write a `.md` file
+1. Use a yaml frontmatter with an attribute of key `core`
+1. The value from this attribute is a shell cmd to execute
+1. Run `imd` it will strip out the code blocks and make an output file
+1. The name of the output file is the name of the `.md` notebook plus the lang following the first code block
+1. `imd` will set a env var named `IMD_TMP` with the output file
+1. `imd` will run the shell cmd
+
